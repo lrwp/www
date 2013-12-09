1537fb5debc35c82c5f10079b934da0f92bcf54d
Travis Paul <Tr@visPaul.me>
2013-12-08 20:01:28 -0500

    cleaned up homepage view

--- src/views/home/map.js
+++ src/views/home/map.js
@@ -2,8 +2,9 @@
 function(doc) {
     'use strict';
 
-    // We don't care about schemaless documents or pages
-    if (doc.schema && doc.schema !== 'page' && doc.created && !doc.archive) {
+    var schemas = ['slide', 'species', 'news', 'brief'];
+
+    if (doc.schema && (schemas.indexOf(doc.schema) !== -1) && doc.created && !doc.archive) {
 
         var obj = {
             title: doc.title,
@@ -19,12 +20,13 @@ function(doc) {
             obj.link = doc.link;
         }
 
-        if (doc.schema === 'species') {
+        if (doc.schema === 'species' && doc.active) {
             obj.common = doc.common;
             obj.content = doc.content;
             obj.type = doc.type;
             obj._attachments = doc._attachments;
             obj.latin = doc.latin;
+            obj.active = doc.active;
         }
 
         emit(key, obj);
