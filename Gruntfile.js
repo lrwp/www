4241dadc87f2e252d65f5814f4d85ec06cc8a9ab
Travis Paul <Tr@visPaul.me>
2015-07-01 20:54:20 -0400

    Setup build directory in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,12 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-mkdir');
 
     grunt.initConfig({
         jshint: {
@@ -8,10 +14,35 @@ module.exports = function(grunt) {
                 'Gruntfile.js',
                 'src/_attachments/js/*.js'
             ]
-        }
+        },
+        mkdir: {
+            build: {
+                options: {
+                    create: ['build']
+                }
+            }
+        },
+        clean: {
+            build: ['build'],
+        },
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ['*', '!nodejs'],
+                        dest: 'build/src'
+                    }
+                ]
+            }
+        },
     });
 
     grunt.registerTask('default', [
+        'clean',
+        'mkdir',
+        'copy',
         'jshint'
     ]);
 
